02af17795cc2123bce18fe52fc864bf468ae1a7f
PabloLUC9832 <lucaspablo340@outlook.com>
2022-05-30 13:42:41 -0500

    First

---
+++ header.php
@@ -0,0 +1,30 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="index.php">Sistema de Préstamos de Equipo de Cómputo UV</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="">Acerca de Nosotros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="">Registro de Solicitud</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="">Lista de Usuarios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="">Bitácora</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="">Observaciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="">Chat</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"/>
+    <title>Home UV</title>
+</head>
+<body>
+
+    <!-- HEADER -->
+    <?php
+    include 'header.php';
+    ?>
+    <!-- HEADER -->
+<!-- 
+    <h1>Registro de </h1>
+
+
+    <button type="button" class="btn btn-primary">Primary</button> -->
+    
+</body>
+</html>
+
