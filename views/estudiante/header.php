2487df0337fc0ce13c14b057317116440c8f1c45
PabloLUC9832 <lucaspablo340@outlook.com>
2022-06-14 00:59:43 -0500

    archivos para rol estudiante

--- views/estudiante/header.php
+++ views/estudiante/header.php
@@ -1,33 +1,28 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="home.php">Sistema de Préstamos de Equipo de Cómputo UV</a>
+        <a class="navbar-brand" href="./home.php">Sistema de Préstamos de Equipo de Cómputo UV</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="">Acerca de Nosotros</a>
+                    <a class="nav-link text-white" href="./acerca.php">Acerca de Nosotros</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="registro_de_solicitud.php">Registro de Solicitud</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="../views/lista_de_usuarios.php">Lista de Usuarios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="../views/bitacora.php">Bitácora</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Prestamos</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="">Solicitar Prestamos</a>
+                        <a class="dropdown-item" href="./lista_de_equipos.php">Ver Equipos</a>
+                    </div>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="">Observaciones</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="../chat/chat.php">Chat</a>
-                </li>                    
-            </ul>
+                    <a class="nav-link text-white" href="./chat.php">Chat</a>
+                </li>
         </div>
-    </nav>
-<!--     <?php date_default_timezone_set('America/Mexico_City'); ?>
-    <p class="text-right"><b><?php echo date('d-m-Y' ); ?></b></p> -->
-   
+
 </header>
